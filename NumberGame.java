406c35677f9061c12f2062da8c299f0f4248db5f
srivatsan-21 <srivathsanj2@gmail.com>
2024-08-15 14:50:39 +0530

    NumberGame

---
+++ NumberGame.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        int userGuess = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Try to guess the number between 1 and 100.");
+
+        while (userGuess != numberToGuess) {
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+
+            if (userGuess < numberToGuess) {
+                System.out.println("Try a higher number.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Try a lower number.");
+            }
+        }
+
+        System.out.println("Congratulations! You guessed the correct number.");
+        scanner.close();
+    68
+}
+}
